b738df7a23cd350307737aaa7ab97d9e567d0581
AliElgohary <alielgohary35@gmail.com>
2024-02-06 03:46:56 +0200

    apply coupon to cart

--- server/src/modules/cart/cart.routes.js
+++ server/src/modules/cart/cart.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createCart, updateCart } from "./controller/cart.controller.js";
+import { applyCoupon, createCart, updateCart } from "./controller/cart.controller.js";
 import { auth } from "../../middlewares/auth.js";
 const cartRouter = express.Router();
 
 cartRouter.post("/cart", auth, createCart);
 
 cartRouter.patch("/cart", auth ,updateCart);
+
+cartRouter.put("/cart/coupon", auth, applyCoupon);
 
 export default cartRouter;
--- server/src/modules/cart/controller/cart.controller.js
+++ server/src/modules/cart/controller/cart.controller.js
@@ -1,6 +1,7 @@
 import cartModel from "../../../../db/models/cart.model.js";
 import productModel from "../../../../db/models/product.model.js";
 import userModel from "../../../../db/models/user.model.js";
+import couponModel from "../../../../db/models/coupon.model.js";
 
 export const createCart = async (req, res) => {
   const user = await userModel.findById(req.userId);
@@ -39,20 +40,40 @@ export const updateCart = async (req, res) => {
       (accumulator, product) => accumulator + product.finalPrice,
       0
     );
-
     const priceAfterDiscount = updatedCart.products.reduce(
       (accumulator, product) => accumulator + product.priceAfterDiscount,
       0
     );
-
     await cartModel.findByIdAndUpdate(cartId, {
       totalPrice: totalPrice,
       priceAfterDiscount: priceAfterDiscount,
     });
     res.send({ message: "success", cart: updatedCart });
   } catch (error) {
-    res
-      .status(500)
-      .send({ error: "Error updating cart", errorMessage: error.message });
+    res.send({ error: "Error updating cart", errorMessage: error.message });
+  }
+};
+
+export const applyCoupon = async (req, res) => {
+  const user = await userModel.findById(req.userId);
+  const { couponCode } = req.body;
+
+  try {
+    const coupon = await couponModel.findOne({ couponCode: couponCode });
+    console.log(coupon)
+    if (!coupon) {
+      return res.send({ message: "Coupon not found" });
+    }
+    const userCart = await cartModel.findOne({ userId: user.id });
+    const priceAfterDiscount = userCart.totalPrice - coupon.value;
+    await cartModel.findByIdAndUpdate(
+      { _id: userCart._id },
+      { $set: { priceAfterDiscount: priceAfterDiscount } },
+      { new: true }
+    );
+    const cart = await cartModel.findOne({ userId: user.id });
+    return res.send({ message: "Cart updated" , cart: cart});
+  } catch (error) {
+    return res.status(500).send({ error: "Error applying coupon", errorMessage: error.message });
   }
 };
